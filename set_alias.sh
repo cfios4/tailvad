f92745fcaffcebb220609cecdc66c35b98d85381
cfios4 <62921651+cfios4@users.noreply.github.com>
2023-06-23 15:12:27 -0400

    Update set_alias.sh
    Append alias to ~/.bashrc

--- set_alias.sh
+++ set_alias.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cat <<EOF > /etc/bashrc
+cat <<EOF >> ~/.bashrc
 alias tailscale="docker exec tailvad tailscale"
 alias wg="docker exec tailvad wg"
 alias mullvad="curl https://am.i.mullvad.net/connected"
